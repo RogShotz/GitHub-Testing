ac008e053e3a5953df13699a53700c298b846e49
lukeroweprivate@gmail.com <lukeroweprivate@gmail.com>
2016-01-23 13:09:56 -0800

    Edited code to go fully up to 100, added sleep, for a cooler output.

--- FizzBuzz/FizzBuzz.cpp
+++ FizzBuzz/FizzBuzz.cpp
@@ -7,13 +7,14 @@
 
 
 #include <iostream>
+#include <windows.h>
 
 
 int main(){
 
 int x;
 
-	for(x = 1; 100>x; x++){
+	for(x = 1; 100>=x; x++){
 
 		if(x % 3 == 0 && x % 5 == 0){
 			std::flush(std::cout) << "FizzBuzz" << std::endl;
@@ -36,7 +37,9 @@ int x;
 				std::flush(std::cout) << x << std::endl;
 
 				}
+		Sleep(100);
 	}
+
 
 
 }
