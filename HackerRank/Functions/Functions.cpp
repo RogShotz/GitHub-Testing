c687879aa8c55c4d68dce183ccfc61df8e59b05d
lukeroweprivate@gmail.com <lukeroweprivate@gmail.com>
2016-01-23 16:18:39 -0800

    Maid Functions.cpp Look prettier

--- HackerRank/Functions/Functions.cpp
+++ HackerRank/Functions/Functions.cpp
@@ -1,15 +1,14 @@
 /*
 * Functions.cpp
 *
-*  Created on: Jan 23, 2016
-*      Author: Luke
+* Created on: Jan 23, 2016
+*  Author: Luke
 */
 
 #include <iostream>
 #include <cstdio>
 
-
-int CHOWmain(){
+int CHOWmain() {
     int a, b, c, d;
 
     std::cin >> a;
@@ -17,26 +16,25 @@ int CHOWmain(){
     std::cin >> c;
     std::cin >> d;
 
-    if(a>b && a>c && a>d){
+    if (a > b && a > c && a > d) {
 
         std::cout << a;
     }
 
-    if(b>a && b>c && b>d){
+    if (b > a && b > c && b > d) {
 
         std::cout << b;
     }
 
-    if(c>a && c>b && c>d){
+    if (c > a && c > b && c > d) {
 
         std::cout << c;
     }
 
-    if(d>a && d>b && d>c){
+    if (d > a && d > b && d > c) {
 
         std::cout << d;
     }
 
     return 0;
 }
-
