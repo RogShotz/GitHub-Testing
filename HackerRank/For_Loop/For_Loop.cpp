ac98037020db068f281580ca09a15d987552e846
lukeroweprivate@gmail.com <lukeroweprivate@gmail.com>
2016-01-23 16:18:14 -0800

    Maid For_Loop.cpp Look prettier.

--- HackerRank/For_Loop/For_Loop.cpp
+++ HackerRank/For_Loop/For_Loop.cpp
@@ -1,85 +1,83 @@
 /*
-* hcountcker.cpp
-*
-*  Crecountted on: Jcountn 23, 2016
-*      countuthor: Luke
-*/
+ * hcountcker.cpp
+ *
+ *  Crecountted on: Jcountn 23, 2016
+ *      countuthor: Luke
+ */
 
 #include <iostream>
 
+int mainG() {
 
-int main(){
+	int a;
+	int b;
+	int count;
 
-    int a;
-    int b;
-    int count;
+	std::cin >> a;
 
-    std::cin >> a;
+	std::cin >> b;
 
-    std::cin >> b;
+	for (count = a; count <= b; count++) {
+		if (count == 1) {
+			std::cout << "one" << std::endl;
 
-    for(count = a; count <= b; count++){
-        if (count == 1){
-            std::cout << "one" << std::endl;
+		}
 
-        }
+		if (count == 2) {
+			std::cout << "two" << std::endl;
 
-        if (count == 2){
-            std::cout << "two" << std::endl;
+		}
 
-        }
+		if (count == 3) {
+			std::cout << "three" << std::endl;
 
-        if (count == 3){
-            std::cout << "three" << std::endl;
+		}
 
-        }
+		if (count == 4) {
+			std::cout << "four" << std::endl;
 
-        if (count == 4){
-            std::cout << "four" << std::endl;
+		}
 
-        }
+		if (count == 5) {
+			std::cout << "five" << std::endl;
 
-        if (count == 5){
-            std::cout << "five" << std::endl;
+		}
 
-        }
+		if (count == 6) {
+			std::cout << "six" << std::endl;
 
-        if (count == 6){
-            std::cout << "six" << std::endl;
+		}
 
-        }
+		if (count == 7) {
+			std::cout << "seven" << std::endl;
 
-        if (count == 7){
-            std::cout << "seven" << std::endl;
+		}
 
-        }
+		if (count == 8) {
+			std::cout << "eight" << std::endl;
 
-        if (count == 8){
-            std::cout << "eight" << std::endl;
+		}
+		if (count == 9) {
+			std::cout << "nine" << std::endl;
 
-        }
-        if (count == 9){
-            std::cout << "nine" << std::endl;
+		}
 
-        }
+		if (count >= 10) {
 
-        if (count >= 10){
+			if (count % 2 == 0) {
+				std::cout << "even" << std::endl;
 
-            if(count % 2 == 0){
-                std::cout << "even" << std::endl;
+			}
 
-            }
+			else {
+				std::cout << "odd" << std::endl;
 
-            else{
-                std::cout << "odd" << std::endl;
+			}
 
-            }
+		}
 
-        }
+	}
 
-    }
-
-
-    return 0;
+	return 0;
 
 }
